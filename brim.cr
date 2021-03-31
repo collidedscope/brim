61f0894755b51eb3b7b3b7d037ed3f33ba04de5e
Collided Scope <collidedscope+github@protonmail.com>
2021-03-31 12:12:18 -0400

    Add zigzag operation

--- brim.cr
+++ brim.cr
@@ -40,6 +40,10 @@ def rotate180(b : UInt8)
   flipy (b & 0xCC) >> 2 | (b & 0x33) << 2
 end
 
+def zigzag(b : UInt8)
+  b & 0x99
+end
+
 SCAN_MASKS = {
   h: 0xCC, hw: 0xF0, v: 0xAA,
   dex0: 0xB4, dex1: 0x4B, sin0: 0x1E, sin1: 0xE1,
@@ -56,7 +60,7 @@ abort USAGE unless ARGV.size >= 2
 file = ARGV.pop
 
 OPERATIONS = {
-  "180", "check", "flipx", "flipy", "invert",
+  "180", "check", "flipx", "flipy", "invert", "zigzag",
   "scandex", "scansin", "scanh", "scanhw", "scanv", "scanvw",
 }
 unless ARGV.all? { |op| OPERATIONS.includes? op }
@@ -95,6 +99,7 @@ ARGV.each do |op|
     when "flipx" ; row.map &->flipx(UInt8)
     when "flipy" ; row.map &->flipy(UInt8)
     when "invert"; row.map &->invert(UInt8)
+    when "zigzag"; row.map &->zigzag(UInt8)
     when "scanh" ; row.map &->scanh(UInt8)
     when "scanhw"; row.map &->scanhw(UInt8)
     when "scanv" ; row.map &->scanv(UInt8)
